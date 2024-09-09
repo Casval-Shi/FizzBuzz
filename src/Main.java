523aff66f2403b7f3fdc2b703bd50ae1328b4e80
unknown <juncheng,shi@mail.utoronto.ca>
2024-09-09 14:20:40 -0400

    make the method public

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            i = rewritefizz.getI(i);
+        }
+    }
 }
